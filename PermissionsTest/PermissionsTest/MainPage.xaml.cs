58e429209e280e48ffa684d1104fec4a67e9a9e6
Francois <francois.mouchati@gmail.com>
2018-04-04 19:03:22 +0200

    Crash
    __CRASHING_DUE_TO_PRIVACY_VIOLATION__

--- PermissionsTest/PermissionsTest/MainPage.xaml.cs
+++ PermissionsTest/PermissionsTest/MainPage.xaml.cs
@@ -1,17 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 using Xamarin.Forms;
 
 namespace PermissionsTest
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+		    TestAsync();
 		}
+
+	    private static async Task TestAsync()
+	    {
+	        var photoSStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Photos);
+	        var mediaStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.MediaLibrary);
+
+	        if (photoSStatus != PermissionStatus.Granted || mediaStatus != PermissionStatus.Granted)
+	        {
+	            var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Photos, Permission.MediaLibrary);
+	            photoSStatus = results[Permission.Photos];
+	            mediaStatus = results[Permission.MediaLibrary];
+	        }
+        }
 	}
 }
